35b772d3610ff9b481c736b35dac72b1c4cd51e9
José J. da Silva Neto <zeneto2508@gmail.com>
2023-03-28 01:20:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var teams_sound = new Audio();
+teams_sound.src = "./assets/sounds/teams.mp3"
+
+var outlook_sound = new Audio();
+outlook_sound.src = "./assets/sounds/outlook.mp3"
+
+var discord_sound = new Audio();
+discord_sound.src = "./assets/sounds/discord.mp3"
+
+var skype_sound = new Audio();
+skype_sound.src = "./assets/sounds/skype.mp3"
+
+var google_calendar_sound = new Audio();
+google_calendar_sound.src = "./assets/sounds/google-calendar.mp3"
+
+var apple_mail_sound = new Audio();
+apple_mail_sound.src = "./assets/sounds/apple-mail.mp3"
+
+var imessage_sound = new Audio();
+imessage_sound.src = "./assets/sounds/imessage.mp3";
+
+var vibrating_phone_sound = new Audio();
+vibrating_phone_sound.src = "./assets/sounds/vibrating-phone.mp3";
